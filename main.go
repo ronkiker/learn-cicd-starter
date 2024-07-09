5bc9c6452c996ce96928c19cd77c1ab1f6067bd7
ronkiker <ron.s.kiker@gmail.com>
2024-07-09 14:43:05 -0600

    fix and cleanup

--- main.go
+++ main.go
@@ -37,8 +37,6 @@ func main() {
 
 	apiCfg := apiConfig{}
 
-	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
-	// libsql://[your-database].turso.io?authToken=[your-auth-token]
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
 		log.Println("DATABASE_URL environment variable is not set")
